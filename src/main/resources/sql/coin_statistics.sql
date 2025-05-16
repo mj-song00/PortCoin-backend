956ffb4f0151db9324885f71624967f00c07a33a
mj-song00 <niveus128@gmail.com>
2025-05-16 22:20:02 +0900

    Chore: 전체 코인량 조회 sql 수정

--- src/main/resources/sql/coin_statistics.sql
+++ src/main/resources/sql/coin_statistics.sql
@@ -1,1 +1,7 @@
-SELECT * FROM
+SELECT c.symbol, SUM(pc.amount) AS sum
+FROM portfolio_coin pc
+         JOIN coin c ON pc.coin_id = c.id
+         JOIN portfolio p ON pc.portfolio_id = p.portfolio_id
+         JOIN users u ON p.user_id = u.id
+GROUP BY c.symbol
+ORDER BY sum DESC;
