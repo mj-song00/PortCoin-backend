92322ce764ede38c015d37ae2bcd36c5a0f59825
mj-song00 <niveus128@gmail.com>
2025-06-11 17:04:32 +0900

    Feat: CORS 설정 추가

--- src/main/java/com/port/portcoin/common/config/WebConfig.java
+++ src/main/java/com/port/portcoin/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.port.portcoin.common.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,5 +16,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키 허용
     }
 }
