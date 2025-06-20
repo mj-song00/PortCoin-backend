37be39daf29aef0d36b300323f0df27bb2806b95
mj-song00 <niveus128@gmail.com>
2025-06-20 17:17:44 +0900

    Chore: wrapper에 chartPoint 생성자 추가

--- src/main/java/com/port/portcoin/domain/chart/dto/response/CoinChartResponseWrapper.java
+++ src/main/java/com/port/portcoin/domain/chart/dto/response/CoinChartResponseWrapper.java
@@ -1,5 +1,6 @@
 package com.port.portcoin.domain.chart.dto.response;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,17 +10,30 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class CoinChartResponseWrapper {
     private List<ChartPoint> prices;
 
+    public CoinChartResponseWrapper(List<ChartPoint> chartPoints) {
+        this.prices = chartPoints;
+    }
+
     @Getter
-    @NoArgsConstructor
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class ChartPoint {
-        private LocalDate date;
+        private String date;
         private double price;
         private String symbol;
         private String name;
+        private double priceChangePercentage24h;
+
+        public ChartPoint(LocalDate date, double price, String symbol,
+                          String name, double priceChangePercentage24h) {
+            this.date = date.toString(); // "2024-06-20"
+            this.price = price;
+            this.symbol = symbol;
+            this.name = name;
+            this.priceChangePercentage24h = priceChangePercentage24h;
+        }
     }
 }
