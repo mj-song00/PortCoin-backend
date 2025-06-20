f7b0b6bd12e85c4c866c6281c1cc133a49348c9d
mj-song00 <niveus128@gmail.com>
2025-06-20 17:14:15 +0900

    Chore: user id 제거

--- src/main/java/com/port/portcoin/domain/portfolio/dto/response/PortfolioResponse.java
+++ src/main/java/com/port/portcoin/domain/portfolio/dto/response/PortfolioResponse.java
@@ -3,16 +3,13 @@ package com.port.portcoin.domain.portfolio.dto.response;
 import com.port.portcoin.domain.portfolio.entity.Portfolio;
 import lombok.Getter;
 
-import java.util.UUID;
 
 @Getter
 public class PortfolioResponse {
-    private final UUID id;
     private final String name;
     private final Long portfolioId;
 
     public PortfolioResponse(Portfolio portfolio){
-        this.id = portfolio.getUser().getId();
         this.name = portfolio.getName();
         this.portfolioId = portfolio.getPortfolioId();
     }
