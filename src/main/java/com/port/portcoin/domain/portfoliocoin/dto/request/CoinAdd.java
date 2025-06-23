a8a372cc78c0b7d58d5ff5100d3a6dd4e79d8d19
mj-song00 <niveus128@gmail.com>
2025-06-23 22:19:33 +0900

    Chore: 코인추가시 purchaseDate의 타입을 LocalDateTime에서 String으로 변경

--- src/main/java/com/port/portcoin/domain/portfoliocoin/dto/request/CoinAdd.java
+++ src/main/java/com/port/portcoin/domain/portfoliocoin/dto/request/CoinAdd.java
@@ -2,13 +2,11 @@ package com.port.portcoin.domain.portfoliocoin.dto.request;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class CoinAdd {
     private Long coinId;
     private Double amount;
     private Double purchasePrice;
     private Double currentPrice;
-    private LocalDateTime purchaseDate;
+    private String purchaseDate;
 }
