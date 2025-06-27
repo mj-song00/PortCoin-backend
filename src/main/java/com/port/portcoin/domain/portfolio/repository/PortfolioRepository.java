399a837063e05599d1e43455952fa090388b8bd6
mj-song00 <niveus128@gmail.com>
2025-06-27 10:32:28 +0900

    Chore: n+1 문제 해결을 위한 fetch join 설정

--- src/main/java/com/port/portcoin/domain/portfolio/repository/PortfolioRepository.java
+++ src/main/java/com/port/portcoin/domain/portfolio/repository/PortfolioRepository.java
@@ -2,13 +2,17 @@ package com.port.portcoin.domain.portfolio.repository;
 
 import com.port.portcoin.domain.portfolio.entity.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public  interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-
+    @Query("SELECT p FROM Portfolio p " +
+            "LEFT JOIN FETCH p.portfolioCoins pc " +
+            "LEFT JOIN FETCH pc.coin " +
+            "WHERE p.portfolioId = :portfolioId")
     Optional<Portfolio> findByPortfolioId(Long portfolioId);
 
     List<Portfolio> findAllByUserIdAndDeletedAtIsNull(UUID id);
